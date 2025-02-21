f6d0f7f35237ce687cc6a61429c207e8380cfab0
Shir <nangiale.shir@sap.com>
2025-02-21 12:29:16 +0100

    Refactor: Use env[] syntax for GitHub Actions input variables

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,14 +7,26 @@ echo "::group::All environment variables"
 env | sort
 echo "::endgroup::"
 
-# Export all INPUT_ variables as environment variables, replacing "-" with "_"
-for var in $(env | grep '^INPUT_' | sed 's/=.*//'); do
-  new_var=$(echo "$var" | sed 's/^INPUT_//' | tr '-' '_')
+export CHANGELOG_FILE_PATH="${env[INPUT_CHANGELOG-FILE-PATH]}"
+echo "CHANGELOG_FILE_PATH=${env[INPUT_CHANGELOG-FILE-PATH]}" | tee -a "$GITHUB_ENV"
 
-  echo "$new_var=${!var}" | tee -a "$GITHUB_ENV"
-  export "$new_var=${!var}"
-done
+export CUSTOM_TAG="${env[INPUT_CUSTOM-TAG]}"
+echo "CUSTOM_TAG=${env[INPUT_CUSTOM-TAG]}" | tee -a "$GITHUB_ENV"
 
+export FALLBACK_VERSION="${env[INPUT_FALLBACK-VERSION]}"
+echo "FALLBACK_VERSION=${env[INPUT_FALLBACK-VERSION]}" | tee -a "$GITHUB_ENV"
+
+export GITHUB_TOKEN="${env[INPUT_GITHUB-TOKEN]}"
+echo "GITHUB_TOKEN=${env[INPUT_GITHUB-TOKEN]}" | tee -a "$GITHUB_ENV"
+
+export RELEASE_DRAFT="${env[INPUT_RELEASE-DRAFT]}"
+echo "RELEASE_DRAFT=${env[INPUT_RELEASE-DRAFT]}" | tee -a "$GITHUB_ENV"
+
+export RELEASE_PRERELEASE="${env[INPUT_RELEASE-PRERELEASE]}"
+echo "RELEASE_PRERELEASE=${env[INPUT_RELEASE-PRERELEASE]}" | tee -a "$GITHUB_ENV"
+
+export RELEASE_TITLE_PREFIX="${env[INPUT_RELEASE-TITLE-PREFIX]}"
+echo "RELEASE_TITLE_PREFIX=${env[INPUT_RELEASE-TITLE-PREFIX]}" | tee -a "$GITHUB_ENV"
 
 echo "::group::All environment variables"
 env | sort
