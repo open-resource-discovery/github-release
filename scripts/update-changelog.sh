e003aab9d280ea4be3f6da107d78467a69fd18d3
Shir <nangiale.shir@sap.com>
2025-02-21 13:43:08 +0100

    Fix: update changelog bug

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -58,39 +58,39 @@ if grep -Eq "^## \\[\\[$VERSION\\]\\]" "$CHANGELOG_FILE_PATH" || \
 
    echo "CHANGELOG_UPDATED=false" | tee -a $GITHUB_ENV
    export CHANGELOG_UPDATED=false
+else
+  # If the version does not exist, update the changelog
+  echo "Version $VERSION not found in changelog.md. Updating changelog..."
+
+  description=$(awk '/^## \[unreleased\]/{flag=1; next} /^## \[/{flag=0} flag' "$CHANGELOG_FILE_PATH")
+
+  header=$(awk '/^## \[unreleased\]/{exit} {print}' "$CHANGELOG_FILE_PATH")
+  rest=$(awk 'BEGIN {found_unreleased=0; found_first_version=0} \
+              /^## \[unreleased\]/ {found_unreleased=1; next} \
+              /^## \[/{if (found_unreleased && !found_first_version) {found_first_version=1} else if (found_first_version) {print; next} } \
+              found_first_version {print}' "$CHANGELOG_FILE_PATH")
+
+  {
+    echo "$header"
+    echo ""
+    echo "## [unreleased]"
+    echo ""
+    echo "## [[$VERSION]($VERSION_LINK)] - $(date +'%Y-%m-%d')"
+    echo "$description"
+    echo ""
+    echo "$rest"
+  } > "$CHANGELOG_FILE_PATH"
+
+  {
+    echo "$description"
+    echo ""
+    echo "### Commits"
+    echo "$commit_log"
+    echo ""
+    echo "### Contributors"
+    echo "$contributors"
+  } > changelog_content.txt
+
+  echo "CHANGELOG_UPDATED=true" | tee -a $GITHUB_ENV
+  export CHANGELOG_UPDATED=true
 fi
-
-# If the version does not exist, update the changelog
-echo "Version $VERSION not found in changelog.md. Updating changelog..."
-
-description=$(awk '/^## \[unreleased\]/{flag=1; next} /^## \[/{flag=0} flag' "$CHANGELOG_FILE_PATH")
-
-header=$(awk '/^## \[unreleased\]/{exit} {print}' "$CHANGELOG_FILE_PATH")
-rest=$(awk 'BEGIN {found_unreleased=0; found_first_version=0} \
-            /^## \[unreleased\]/ {found_unreleased=1; next} \
-            /^## \[/{if (found_unreleased && !found_first_version) {found_first_version=1} else if (found_first_version) {print; next} } \
-            found_first_version {print}' "$CHANGELOG_FILE_PATH")
-
-{
-  echo "$header"
-  echo ""
-  echo "## [unreleased]"
-  echo ""
-  echo "## [[$VERSION]($VERSION_LINK)] - $(date +'%Y-%m-%d')"
-  echo "$description"
-  echo ""
-  echo "$rest"
-} > "$CHANGELOG_FILE_PATH"
-
-{
-  echo "$description"
-  echo ""
-  echo "### Commits"
-  echo "$commit_log"
-  echo ""
-  echo "### Contributors"
-  echo "$contributors"
-} > changelog_content.txt
-
-echo "CHANGELOG_UPDATED=true" | tee -a $GITHUB_ENV
-export CHANGELOG_UPDATED=true
